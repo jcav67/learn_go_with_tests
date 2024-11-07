ce2b0fd721228a31c415f31840a3156ae36f0bbd
Camilo Arango <jmilo67@hotmail.com>
2024-11-06 19:33:45 -0500

    :sparkle: DI and mockery

---
+++ dependecy_injections/greet.go
@@ -0,0 +1,15 @@
+package dependency_injections
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Printf("Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+}

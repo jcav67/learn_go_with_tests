b47d459bfd58a2424e1cc0af03ed367969b9e9f0
Camilo Arango <jmilo67@hotmail.com>
2024-11-18 01:15:56 -0500

    :memo: concurrency and mockery 2

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -13,16 +13,23 @@ const countdownStart = 3
 type Sleeper interface {
 	Sleep()
 }
-
-type SpySleeper struct {
-	Calls int
-}
-
 type DefaultSleeper struct{}
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+type SpyCountdownOperations struct {
+	Calls []string
 }
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
